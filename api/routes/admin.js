521bd42be9d34a508b2c0545c01fb3f9335ce435
Satyajeet Singh <satyajeet104@gmail.com>
2018-06-09 10:05:39 +0530

    setting up image upload and tesseract

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -2,41 +2,89 @@ const express = require('express')
 const router = express.Router()
 const db = require('./../../db')
 const formidable = require('formidable');
+const mkdirp = require('mkdirp');
 
 
-router.get('/',(req,res)=> {
-   
+
+router.get('/', (req, res) => {
+
     res.render("index1");
-    
+
+
+})
+
+router.post('/recog', function (req, res) {
+
+    var promise1 = new Promise(function (resolve, reject) {
+        var form = new formidable.IncomingForm();
+
+
+        form.parse(req);
+
+
+
+
+        form.on('fileBegin', function (name, file) {
+            file.path = __dirname + '/../../public/uploads/temp/' + file.name;
+            console.log(file.path);
+            form.on('file', function (name, file) {
+                console.log('Uploaded ' + file.name);
+
+            });
+
+        });
+
+
+
+
+    })
+
+    promise1.then(function (data) {
+        console.log("resolved");
+    })
+
+
+
+
+    res.render("index1");
 
 })
 
 
-router.post('/', function (req, res){
+router.post('/uploadFile', function (req, res) {
 
-    var promise1=new Promise(function(resolve,reject){
+    console.log(req.body.student)
+    var promise1 = new Promise(function (resolve, reject) {
         var form = new formidable.IncomingForm();
 
+
         form.parse(req);
-    
-        form.on('fileBegin', function (name, file){
-            file.path = __dirname + '/public/uploads/' + file.name;
+
+
+
+
+        form.on('fileBegin', function (name, file) {
+            file.path = __dirname + '/../../public/uploads/' + file.name;
+            console.log(file.path);
+            form.on('file', function (name, file) {
+                console.log('Uploaded ' + file.name);
+
+            });
+
+
         });
-    
-        form.on('file', function (name, file){
-            console.log('Uploaded ' + file.name);
-            resolve();
-        });
-    
+
+
+
 
     })
 
-    promise1.then(function(data){
+    promise1.then(function (data) {
         console.log("resolved");
 
 
     })
-      
-    
+
+
 });
-exports=module.exports = router
+exports = module.exports = router
